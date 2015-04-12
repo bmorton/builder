a4edebfd6b04ec1f80ab88f9ead76d6f65c4784a
Brian Morton <brian@mmm.hm>
2015-04-12 00:05:16 -0700

    Add simple tests for Build and State in builds package.

---
+++ builds/build_test.go
@@ -0,0 +1,20 @@
+package builds
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDefaultName(t *testing.T) {
+	b := New("", "https://github.com/bmorton/deployster", "", "")
+	b.SetDefaultName()
+	assert.Equal(t, "deployster", b.RepositoryName)
+}
+
+func TestStateMarshaling(t *testing.T) {
+	s := Waiting
+	actual, err := s.MarshalText()
+	assert.Nil(t, err)
+	assert.Equal(t, []byte("Waiting"), actual)
+}
